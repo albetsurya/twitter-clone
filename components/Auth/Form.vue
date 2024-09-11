808f6636b6832b1abf5a983e73c827600d0901ce
= <albetsurya66@gmail.com>
2024-09-11 15:09:58 +0700

    feat: useAuth

--- components/Auth/Form.vue
+++ components/Auth/Form.vue
@@ -1,10 +1,45 @@
-<script setup></script>
+<script setup>
+const data = reactive({
+  username: "",
+  password: "",
+  loading: false,
+});
+
+async function handleLogin() {
+  const { login } = useAuth();
+  data.loading = true;
+  try {
+    await login({
+      username: data.username,
+      password: data.password,
+    });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    data.loading = false;
+  }
+}
+</script>
 
 <template>
   <div>
     <div class="pt-5 space-y-6">
-      <UIInput label="Username" placeholder="@username" />
-      <UIInput label="Password" placeholder="********" type="password" />
+      <UIInput
+        type="text"
+        label="Username"
+        placeholder="@username"
+        v-model="data.username"
+      />
+
+      <UIInput
+        type="password"
+        label="Password"
+        placeholder="********"
+        v-model="data.password"
+      />
+      <div>
+        <button @click="handleLogin">Login</button>
+      </div>
     </div>
   </div>
 </template>
